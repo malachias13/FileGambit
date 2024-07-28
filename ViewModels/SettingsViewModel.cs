e3844c436354e3c36f1da364541008fe88b1dfbe
Malachias Harris <69220988+malachias13@users.noreply.github.com>
2024-07-28 05:05:42 -0400

    Setup the xmal for the background img setting view

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace PhotoGallery.ViewModels
 {
     internal class SettingsViewModel : ViewModelBase
     {
+        public ObservableCollection<string> bgStretchSettings { get; set; }
         public SettingsViewModel() 
         {
+            bgStretchSettings = new ObservableCollection<string>();
 
+            bgStretchSettings.Add("None");
+            bgStretchSettings.Add("Fill");
+            bgStretchSettings.Add("Uniform");
+            bgStretchSettings.Add("UniformToFill");
         }
     }
 }
